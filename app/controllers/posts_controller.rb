ed51cfd03ba9b0a2f15e764162ca1d5a3d5c0fc7
shumin huang <ms.huang.shumin@gmail.com>
2017-08-10 11:34:16 +0800

    auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
 
   # before_action :authenticate_user!, only: [:new, :edit]
-
-  before_action :authenticate_user!, except: [:index]
-
+  # before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
 
   def index
-    render json: current_user
+    @all_posts = current_user.posts
+    # render html: "show all post by #{current_user.name}"
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  User.create(
+  name: "User#{i+1}",
+  email: "user#{i+1}@gmail.com",
+  password: "user123",
+  password_confirmation: "user123"
+  )
+end
+
+# 5.times do |i|
+#   Post.create(
+#   title: "Post# #{i+1}",
+#   content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+#   user_id: 8
+#   )
+# end
