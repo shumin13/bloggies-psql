cfc507f08ff36a9cf41140e9d90b8fa360766384
shumin huang <ms.huang.shumin@gmail.com>
2017-08-11 12:20:18 +0800

    latest migration for all

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,11 @@ ActiveRecord::Schema.define(version: 20170810075530) do
     t.integer "upvote"
     t.integer "downvote"
     t.bigint "post_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -43,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170810075530) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.string "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -67,5 +70,6 @@ ActiveRecord::Schema.define(version: 20170810075530) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
